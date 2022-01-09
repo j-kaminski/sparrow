2388598d350fdc560ea5a3984fa69635ba1e0339
Wiktor Waj <wiktor.waj@gmail.com>
2022-01-09 20:42:46 +0100

    Initial draft of messenging app with django channels

---
+++ server/sparrowServer/chat/urls.py
@@ -0,0 +1,13 @@
+# chat/urls.py
+from django.urls import path
+
+from .views import index, room
+
+
+
+app_name = 'chat'
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('<str:room_name>/', room, name='room'),
+]
