23595e972ad29e1123deda627d03c6c77d11495a
Jakub Kaminski <j.kaminski@ap-tech.pl>
2022-01-17 18:45:04 +0100

    Add base theme

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,13 +5,31 @@ import { App } from './App';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 import * as serviceWorker from './serviceWorker';
+
+const theme = createTheme({
+	palette: {
+		text: {
+			primary: '#FFFFFF',
+		},
+		primary: {
+			main: '#FFBF00',
+		},
+		secondary: {
+			main: '#464040',
+		},
+	},
+});
 
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<BrowserRouter>
-				<App />
+				<ThemeProvider theme={theme}>
+					<App />
+				</ThemeProvider>
 			</BrowserRouter>
 		</Provider>
 	</React.StrictMode>,
